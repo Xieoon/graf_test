9289729fc9c18d5c3762a1492ee9c04133d56a33
vasya <malahoval99@mail.ru>
2023-05-04 14:34:58 +0300

    firstCommit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import {CategoryScale,LineController,LineElement,PointElement,LinearScale,Title} from 'chart.js';
+import { Line } from "react-chartjs-2";
+import { data } from "./data";
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+ChartJS.register(LineController, LineElement, PointElement, LinearScale, Title, CategoryScale);
 
 function App() {
+const info={
+  xInfo:[],
+  yInfo:[]
+}
+data.data.result[0].values.forEach(element => {
+  info.xInfo.push(element[0])
+  info.yInfo.push(element[1])
+});
+console.log(info);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   
+      <Line
+        datasetIdKey="id"
+        data={{
+          labels: info.xInfo,
+          datasets: [
+            {
+              id: 1,
+              label: "22",
+              data: info.yInfo,
+            },
+            {
+              id: 2,
+              label: "12",
+              data: [3, 2, 1],
+            },
+          ],
+        }}
+      />
   );
 }
 
